73d4aa5194596c49acbb33bcbed8c1e12180b9eb
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-09-22 19:11:34 +0300

    backend create new role_permission

--- backend/routes/roles.js
+++ backend/routes/roles.js
@@ -4,9 +4,14 @@ const rolesRouter = express.Router();
 
 //controllers
 // create New Role
+// http://localhost:5000/roles
 rolesRouter.post("/", roles.createNewRole);
 
 // create New permission
+// http://localhost:5000/roles/permission
 rolesRouter.post("/permission", roles.createNewPermission);
+
+// create new role_permission
+rolesRouter.post("/role_permission", roles.createNewRolePermission);
 
 module.exports = rolesRouter;
