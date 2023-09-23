1fd224bdbea3acc1d6f487cd849b9af15aca80f6
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-09-23 18:27:30 +0300

    backend register

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const users = require("../controllers/users");
 
 //controllers
 
 const usersRouter = express.Router();
 
+// register
+// http://localhost:5000/users/register
+usersRouter.post("/register", users.register);
 module.exports = usersRouter;
