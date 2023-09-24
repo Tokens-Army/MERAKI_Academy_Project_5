700a567fb40c1979fb0f33518054288ae2f0a0c1
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-09-24 17:35:04 +0300

    added CreateOrderById controller and router and modified authorization

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { createOrderById } = require("../controllers/orders");
 const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
 //controllers
 
-
 const ordersRouter = express.Router();
-ordersRouter.post("/:id", authentication, createOrderById);
+ordersRouter.post("/:id", authentication, authorization("ADD_ORDER"),createOrderById);
 
 module.exports = ordersRouter;
