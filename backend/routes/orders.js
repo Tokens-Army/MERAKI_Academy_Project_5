5d76f630b77b2861748db722515f1e090541fde6
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-09-24 18:18:09 +0300

    added getAllOrders controller and router

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createOrderById } = require("../controllers/orders");
+const { createOrderById, getAllOrders } = require("../controllers/orders");
 const authentication = require("../middlewares/authentication");
 const authorization = require("../middlewares/authorization");
 //controllers
 
 const ordersRouter = express.Router();
 ordersRouter.post("/:id", authentication, authorization("ADD_ORDER"),createOrderById);
+ordersRouter.get("", authentication, getAllOrders);
 
 module.exports = ordersRouter;
