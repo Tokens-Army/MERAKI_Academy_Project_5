58534ac64c96e21d204b6fa7c635d69ed4fbb84a
AliIkmeil <ikmeil.ali@gmail.com>
2023-09-24 20:30:50 +0300

    added authintication and authirization for the accessories

--- backend/routes/accessories.js
+++ backend/routes/accessories.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const {createNewAccessories,getAllAccessories,deleteAccessoryById,updateAccessoryById}=require("../controllers/accessories");
 const authentication = require("../middlewares/authentication");
+const authorization = require("../middlewares/authorization");
 
 //controllers
 
 const accessoriesRouter = express.Router();
-accessoriesRouter.post("/",createNewAccessories)
+accessoriesRouter.post("/",authentication,authorization("CREATE_ACCESSORY"),createNewAccessories)
 accessoriesRouter.get("/",getAllAccessories)
-accessoriesRouter.delete("/:id",deleteAccessoryById)
-accessoriesRouter.put("/:id",updateAccessoryById)
+accessoriesRouter.delete("/:id",authentication,authorization("CREATE_ACCESSORY"),deleteAccessoryById)
+accessoriesRouter.put("/:id",authentication,authorization("CREATE_ACCESSORY"), updateAccessoryById)
 
 module.exports = accessoriesRouter;
