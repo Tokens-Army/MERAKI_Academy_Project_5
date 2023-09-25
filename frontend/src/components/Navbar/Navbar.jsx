0bf657ed7371f70d92dca9210affea3ebd3241f0
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-09-25 21:12:33 +0300

    create Navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import "./navbar.css";
 const Navbar = () => {
-  return <div>hello im navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <h3>Spotless Wash</h3>
+      </div>
+      <div className="navbar-search">
+        <input placeholder="Search" />
+      </div>
+      <div>
+        <h3 >Home</h3>
+      </div>
+      <div>
+        <h3>About </h3>
+      </div>
+      <div>
+        <h3>Contact Us</h3>
+      </div>
+      <div>
+        <h3>Login</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
