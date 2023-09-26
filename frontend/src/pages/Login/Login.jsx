98e579505cfb88fcb2fe96a9ff70fcef90f73d7a
SaadHabashneh <Saad.K.Habashneh@gmail.com>
2023-09-27 00:05:10 +0300

    added login component in the react app

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,43 @@
 import React, { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
 import { setLogin, setUserId } from "../../service/redux/loginSlice";
-import "./login.css";
 import axios from "axios";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const Copyright = (props) => {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" to="/">
+        Wash My Ride
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+};
+
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
 
 const Login = () => {
+  
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -37,43 +69,92 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <div>
-        <h4 className="loginTitle">Login</h4>
-      </div>
-      <div className="login-inputs">
-        <input
-          type="email"
-          placeholder="Your email..."
-          onChange={(e) => setEmail(e.target.value)}
+    <ThemeProvider theme={defaultTheme}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         />
-        <br />
-        <br />
-        <input
-          type="password"
-          placeholder="Your password..."
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <br />
-      <div className="loginMessageDiv">
-        {status
-          ? message && <div className="successMsg">{message}</div>
-          : message && <div className="errorMsg">{message}</div>}
-      </div>
-      <br />
-      <div className="loginBtnDiv">
-        <button className="loginBtn" onClick={loginHandler}>
-          Login
-        </button>
-      </div>
-      <br />
-      <div>
-        <span>don't have an account ?</span>
-        <br />
-        <Link to="/register">Register</Link>
-      </div>
-    </div>
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {status
+                ? message && <Typography color="primary">{message}</Typography>
+                : message && <Typography color="error">{message}</Typography>}
+              <br />
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={loginHandler}
+              >
+                Sign In
+              </Button>
+              <br />
+              <Typography>
+                Don't have an account?
+                <Link to="/register" className="noAccLink"> Register</Link>
+              </Typography>
+              <Grid container>
+                <Grid item xs></Grid>
+                <Grid item></Grid>
+              </Grid>
+              <Copyright sx={{ mt: 3 }} />
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
