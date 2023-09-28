ff40f35a1d6593758b5f71dc54a2067f7badeeee
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-09-28 23:59:59 +0300

    added map to the schedule order

---
+++ frontend/src/pages/Location/Location.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}❌</div>;
+
+const Location = () => {
+  const defaultProps = {
+    center: {
+      lat: 31.92425,
+      lng: 35.917441,
+    },
+    zoom: 11,
+  };
+
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+        yesIWantToUseGoogleMapApiInternals
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+          icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+        />
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Location;
