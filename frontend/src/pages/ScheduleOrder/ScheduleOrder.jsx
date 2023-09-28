7455b54e143d0f9c675aac1eb943ef70bc9a80aa
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-09-28 15:48:59 +0300

    date picker is ready

--- frontend/src/pages/ScheduleOrder/ScheduleOrder.jsx
+++ frontend/src/pages/ScheduleOrder/ScheduleOrder.jsx
@@ -1,8 +1,32 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-const ScheduleOrder = () => {
-  const [selectDate, setSelectDate] = useState("");
-  return <></>;
-};
+import * as React from "react";
+import dayjs from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { StaticDateTimePicker } from "@mui/x-date-pickers/StaticDateTimePicker";
 
-export default ScheduleOrder;
+export default function ResponsiveDateTimePickers() {
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer
+        components={[
+          "DateTimePicker",
+          "MobileDateTimePicker",
+          "DesktopDateTimePicker",
+          "StaticDateTimePicker",
+        ]}
+      >
+        <DemoItem label="Static variant">
+          <StaticDateTimePicker
+            defaultValue={dayjs("2023-08-28T15:30")}
+            onChange={(newvalue) => {
+              const arr = String(newvalue.$d).split(" ");
+              arr.splice(5, 2);
+              console.log(arr.join(" "));
+            }}
+          />
+        </DemoItem>
+      </DemoContainer>
+    </LocalizationProvider>
+  );
+}
