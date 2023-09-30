c2fb7aea088e7841767ca5ed25f098d0c0323d95
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-09-30 21:33:35 +0300

    changing location done

--- frontend/src/pages/Location/Location.jsx
+++ frontend/src/pages/Location/Location.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import GoogleMapReact from "google-map-react";
 
-const AnyReactComponent = ({ text }) => <div>{text}❌</div>;
+const Marker = ({ text }) => <h1>📍</h1>;
 
 const Location = (props) => {
+  const [myLoc, setMyLoc] = useState({ lat: 31.92425, lng: 35.917441 });
   const defaultProps = {
     center: {
       lat: 31.92425,
@@ -15,18 +16,34 @@ const Location = (props) => {
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: "100vh", width: "100%" }}>
-      <GoogleMapReact
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-        yesIWantToUseGoogleMapApiInternals
-      >
-        <AnyReactComponent
-          lat={31.92425}
-          lng={35.917441}
-          text="My Marker"
-          icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
-        />
-      </GoogleMapReact>
+      {myLoc && (
+        <GoogleMapReact
+          center={myLoc}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+          yesIWantToUseGoogleMapApiInternals={true}
+          // onGoogleApiLoaded={({ map, maps }) => {
+          //   console.log(myLoc);
+          //   new maps.Marker({
+          //     position: { lat: myLoc.lat, lng: myLoc.lng },
+          //     map,
+          //     center: { lat: myLoc.lat, lng: myLoc.lng },
+          //     title: "hellooooooooooooooooo",
+          //   });
+          // }}
+          onClick={(e) => {
+            // console.log(e, { lat: e.lat, lng: e.lng });
+            setMyLoc({ lat: e.lat, lng: e.lng });
+          }}
+        >
+          <Marker
+            lat={myLoc.lat}
+            lng={myLoc.lng}
+            text="My Marker"
+            // icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+          />
+        </GoogleMapReact>
+      )}
     </div>
   );
 };
