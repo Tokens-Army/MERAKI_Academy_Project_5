c0ed4fc1cd7b4a37c3734b094011a80dc6af50b2
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-10-02 22:32:14 +0300

    accessory page in admin dashboard

--- frontend/src/pages/Admin/Addaccessories/Addaccessories.jsx
+++ frontend/src/pages/Admin/Addaccessories/Addaccessories.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAccessories } from "../../../service/redux/accessorySlice";
+import axios from "axios";
 
 const Addaccessories = () => {
-  const navigate = useNavigate()
-    return (
-    <div>Addaccessories
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const accessories = useSelector((state) => {
+    return state.accessories.accessories;
+  });
 
-    <button onClick={()=>{
-        navigate("/admin")
-    }}>Back to home page</button>
-    </div>
-  )
-}
+  const getAllAccessories = () => {
+    axios
+      .get("http://localhost:5000/accessories")
+      .then((result) => {
+        console.log(result.data.result);
+        dispatch(setAccessories(result.data.result));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getAllAccessories();
+  }, []);
+  return (
+    <>
+      {console.log(accessories)}
+      {accessories &&
+        accessories.map((accessory) => {
+          // return <img src={accessory.img} width="20px" />;
+        })}
+    </>
+  );
+};
 
-export default Addaccessories
+export default Addaccessories;
