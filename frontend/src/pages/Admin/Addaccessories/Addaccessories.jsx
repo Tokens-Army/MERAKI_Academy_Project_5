e431cf9209737d4850aacb9ddc10acf2152b68e9
MohAlawneh <mohammadwalid.alawneh@gmail.com>
2023-10-02 23:25:33 +0300

    editing Admin dashboard Accessories

--- frontend/src/pages/Admin/Addaccessories/Addaccessories.jsx
+++ frontend/src/pages/Admin/Addaccessories/Addaccessories.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect } from "react";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { setAccessories } from "../../../service/redux/accessorySlice";
 import axios from "axios";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 const Addaccessories = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -32,13 +44,60 @@ const Addaccessories = () => {
     getAllAccessories();
   }, []);
   return (
-    <>
-      {console.log(accessories)}
-      {accessories &&
-        accessories.map((accessory) => {
-          // return <img src={accessory.img} width="20px" />;
-        })}
-    </>
+    // const columns = [
+    //   { id: "id", label: "Accessory id", minWidth: 20 },
+    //   { id: "name", label: "Accessory Name", minWidth: 100 },
+    //   {
+    //     id: "description",
+    //     label: "Accessory Desc.",
+    //     minWidth: 170,
+    //     align: "right",
+    //     format: (value) => value.toLocaleString("en-US"),
+    //   },
+    //   {
+    //     id: "img",
+    //     label: "Accessory Img",
+    //     minWidth: 170,
+    //     align: "right",
+    //     format: (value) => value.toLocaleString("en-US"),
+    //   },
+    //   {
+    //     id: "Accessory price",
+    //     label: "price",
+    //     minWidth: 170,
+    //     align: "right",
+    //     format: (value) => value.toFixed(2),
+    //   },
+    // ];
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Accessory id</TableCell>
+            <TableCell align="right">Accessory Name</TableCell>
+            <TableCell align="right">Accessory Desc.</TableCell>
+            <TableCell align="right">Accessory Img</TableCell>
+            <TableCell align="right">Accessory price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
